c0dee3d415f49ee05da6f935ccb35551dc14aa71
shridhar <shridharkur@gmail.com>
2025-01-25 18:57:49 +0530

    updated database

--- models/models.py
+++ models/models.py
@@ -1,25 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy # Import SQLAlchemy from flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
-db = SQLAlchemy()  # Create an instance of SQLAlchemy class
-
+db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False,unique=True)
-    email = db.Column(db.String(40),nullable=False,unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    post = db.relationship("Post", backref="user",lazy=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    roles = db.Column(db.String(50), nullable=False, default='user')
 
 class Post(db.Model):
-    __tablename__ = "posts"
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     flagged = db.Column(db.Boolean, default=False)
-
-
-
-
