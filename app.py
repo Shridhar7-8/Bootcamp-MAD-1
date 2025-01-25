c6f73d60ff1c308dca0c04668c5e12b43e320ddc
shridhar <shridharkur@gmail.com>
2025-01-25 20:37:02 +0530

    Implemented user registration and admin login functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from controllers.controllers import controllers
 from models.models import *
 
 
@@ -9,9 +10,27 @@ app.config['SECRET_KEY'] = "thivya"
 
 db.init_app(app) # Initialize the database with the app
 
+app.register_blueprint(controllers)
+
+def create_admin():
+    with app.app_context():
+        admin_email = "admin@example.com"
+        admin_user = User.query.filter_by(email = admin_email).first()
+        if not admin_user:
+            admin_username = "admin"
+            admin_password = "admin123"
+            admin_user = User(username=admin_username, email=admin_email, password = admin_password, roles="admin")
+            db.session.add(admin_user)
+            db.session.commit()
+            print("Admin created successfully")
+        else:
+            print("Admin already exists")
+
+
 
 with app.app_context():
     db.create_all()
+    create_admin()
 
 
 
